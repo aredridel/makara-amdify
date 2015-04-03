66fd43f6ec0f852a3552cfc891ba85f961bc2035
Aria Stewart <aredridel@dinhe.net>
2015-04-03 17:00:06 -0400

    Stop ending the first test too soon

--- test/index.js
+++ test/index.js
@@ -8,6 +8,8 @@ var bksb = require('../');
 
 test('build', function (t) {
     var root = path.resolve(__dirname, 'fixture');
+    t.plan(4);
+
     bksb.build(root, function (err) {
         t.error(err, 'no error');
 
@@ -23,10 +25,10 @@ test('build', function (t) {
 
             t.ok(result._languagepack['en-XC'], "found our language in the output");
             t.equal(result._languagepack['en-XC']['index.properties'].hello, 'World', "found translation");
+            t.end();
         });
 
     });
-    t.end();
 });
 
 test('languge pack path', function (t) {
