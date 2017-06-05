8cb317b03d51f6c653af71d4740ed4eaaea7170f
escproxy <escproxy@gmail.com>
2017-06-05 14:05:22 +0100

    Updated the the makara-builder construtor with new option syntax

--- build.js
+++ build.js
@@ -3,16 +3,20 @@
 var path = require('path');
 var fs = require('fs');
 
-var wrap = function (out) {
+var wrap = function(out) {
 	return 'define("_languagepack", function () { return ' + JSON.stringify(out) + '; });';
 };
 
-var writer = function (outputRoot, cb) {
-	return function (out) {
+var writer = function(outputRoot, cb) {
+	return function(out) {
 		fs.writeFile(path.resolve(outputRoot, '_languagepack.js'), wrap(out), cb);
 	};
 };
 
-module.exports = function build(root, cb) {
-	require('makara-builder')(root, writer, cb);
+module.exports = function build(appRoot, cb) {
+	require('makara-builder')({
+		appRoot: root,
+		writer: writer,
+		cb: cb
+	});
 };
